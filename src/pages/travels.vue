cb8b75702d3d33399b342ed445b97bb5871ee966
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2024-07-10 00:09:24 +0200

    feat: add travels table

--- src/pages/travels.vue
+++ src/pages/travels.vue
@@ -1,5 +1,34 @@
 <template>
   <div>
-    TRAVELS
+    <DataTable
+      :items="data ?? []"
+      :loading="status === 'pending'"
+      :error="status === 'error'"
+    >
+      <template #[`item.departureDate`]="{ item }">
+        {{ formatDate(item.departureDate) }}
+      </template>
+      <template #[`item.returnDate`]="{ item }">
+        {{ formatDate(item.returnDate) }}
+      </template>
+      <template #[`item.price`]="{ item }">
+        {{ formatPrice(item.price) }}
+      </template>
+    </DataTable>
   </div>
 </template>
+
+<script lang="ts" setup>
+import DataTable from '~/components/table/DataTable.vue'
+import { travelsClient } from '~/resources/travels'
+import { useResourceClientFetch } from '~/resources/useResourceClientFetch'
+import { formatDate, formatPrice } from '~/utils'
+
+useHead({
+  title: 'Travels',
+})
+
+const { data, status } = useResourceClientFetch(travelsClient, {
+  method: 'GET',
+})
+</script>
