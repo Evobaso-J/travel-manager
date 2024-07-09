5990c73e28ced18ac22dae926ae192fee85725af
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2024-07-09 18:43:09 +0200

    feat: create base card component

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="`bg-white shadow-md rounded-lg overflow-scroll ${twClass}`">
+    <slot />
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { defineComponent } from 'vue'
+import type { WithTailwindClass } from '~/types'
+
+defineComponent({
+  name: 'BaseCard',
+})
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type BaseCardProps = WithTailwindClass & {}
+
+defineProps<BaseCardProps>()
+</script>
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export type WithTailwindClass = {
+  twClass?: string
+}
