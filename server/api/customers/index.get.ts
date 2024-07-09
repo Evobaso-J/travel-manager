879150e469e8e7db49a6cfce5aa032ddfc79632f
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2024-07-09 10:28:46 +0200

    feat: add server layer

---
+++ server/api/customers/index.get.ts
@@ -0,0 +1,9 @@
+import type { CustomerResponse } from '~/resources/customers/types/response'
+
+export default defineEventHandler(async (event): Promise<CustomerResponse[]> => {
+  const query = getQuery(event)
+  return await $fetch<CustomerResponse[]>('https://my-json-server.typicode.com/Evobaso-J/travel-manager/customers', {
+    method: 'GET',
+    query,
+  })
+})
--- src/resources/bookings/types/internal.ts
+++ src/resources/bookings/types/internal.ts
@@ -1,7 +1,7 @@
 import type { Customer } from '~/resources/customers/types/internal'
 import type { Travel } from '~/resources/travels/types/internal'
 
-export type PaymentType = 'creditTransfer' | 'payPal' | 'revolute'
+export type PaymentType = 'creditTransfer' | 'payPal' | 'revolut'
 
 export type Booking = {
   id?: number
