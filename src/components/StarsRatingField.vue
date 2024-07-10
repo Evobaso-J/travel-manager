0440f010f4a16e86e302078d3a48f0050af90510
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2024-07-10 02:27:47 +0200

    feat: add star rating

---
+++ src/components/StarsRatingField.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <FontAwesomeIcon
+      v-for="i in 5"
+      :key="i"
+      :icon="{ prefix: 'fas', iconName: 'star' }"
+      :class="i <= value ? 'text-secondary-500' : 'text-gray-300'"
+    />
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+defineComponent({ name: 'StarsRatingField' })
+type StarsRatingFieldProps = {
+  value: number
+}
+defineProps<StarsRatingFieldProps>()
+</script>
