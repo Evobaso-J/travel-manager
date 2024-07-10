778a5e1577ef3cb946adb7384fd5e8a82a5886c3
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2024-07-10 07:26:24 +0200

    feat: add bookings table

--- src/pages/bookings.vue
+++ src/pages/bookings.vue
@@ -1,5 +1,75 @@
 <template>
-  <div>
-    BOOKINGS
-  </div>
+  <DataTable
+    :items="data ?? []"
+    :loading="status === 'pending'"
+    :error="status === 'error'"
+    :hidden-columns="['id']"
+  >
+    <template #[`item.customer`]="{ item }">
+      <div class="flex flex-col gap-2">
+        <div class="flex gap-4 items-center">
+          <GenderField :gender="item.customer.gender" />
+          <span
+            class="font-semibold"
+          >
+            {{ item.customer.name }}
+          </span>
+          <div class="flex items-center gap-1">
+            <IconField
+              :icon="{ prefix: 'fas', iconName: 'birthday-cake' }"
+              :text="String(item.customer.age)"
+            />
+          </div>
+        </div>
+        <div
+          class="flex gap-4 items-center"
+        >
+          <IconField
+            :icon="{ prefix: 'fas', iconName: 'envelope' }"
+            :text="item.customer.email"
+          />
+          <IconField
+            :icon="{ prefix: 'fas', iconName: 'phone' }"
+            :text="item.customer.phone"
+            icon-class="text-green-600"
+          />
+        </div>
+      </div>
+    </template>
+    <template #[`item.travel`]="{ item }">
+      {{ item.travel.name }}
+    </template>
+
+    <template #[`item.paymentType`]="{ item }">
+      {{ mapPaymentTypeToString(item.paymentType) }}
+    </template>
+
+    <template #[`item.notes`]="{ item }">
+      <TextTooltip
+        :text="item.notes"
+        position="top"
+      >
+        <div class="truncate max-w-56">
+          {{ item.notes }}
+        </div>
+      </TextTooltip>
+    </template>
+  </DataTable>
 </template>
+
+<script lang="ts" setup>
+import GenderField from '~/components/fields/GenderField.vue'
+import IconField from '~/components/fields/IconField.vue'
+import DataTable from '~/components/table/DataTable.vue'
+import { bookingsClient } from '~/resources/bookings'
+import { useResourceClientFetch } from '~/resources/useResourceClientFetch'
+import { mapPaymentTypeToString } from '~/utils'
+
+useHead({
+  title: 'Bookings',
+})
+
+const { data, status } = useResourceClientFetch(bookingsClient, {
+  method: 'GET',
+})
+</script>
