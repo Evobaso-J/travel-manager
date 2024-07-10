98733b3ca62725e6a5a62ff7f3db9fc92eabca1a
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2024-07-10 04:17:13 +0200

    feat: add text input

---
+++ src/components/inputs/BaseInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="flex items-center w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus-within:border-primary-500"
+    :class="{ 'cursor-not-allowed': disabled, [twClass ?? '']: true }"
+  >
+    <FontAwesomeIcon
+      v-if="prependIcon"
+      :icon="prependIcon"
+      class="mr-2"
+    />
+    <input
+      v-bind="$attrs"
+      :type
+      class="group w-full h-full bg-transparent focus:outline-none"
+      :disabled
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  FontAwesomeIcon,
+  type FontAwesomeIconProps,
+} from '@fortawesome/vue-fontawesome'
+import type { WithTailwindClass } from '~/types'
+
+defineComponent({ name: 'BaseInput' })
+export type BaseInputProps = {
+  type: HTMLInputElement['type']
+  disabled?: boolean
+  prependIcon?: FontAwesomeIconProps['icon']
+} & WithTailwindClass
+defineProps<BaseInputProps>()
+
+type BaseInputSlots = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  prependIcon: () => any
+}
+defineSlots<BaseInputSlots>()
+</script>
---
+++ src/components/inputs/TextInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <BaseInput
+    v-model="modelValue"
+    type="text"
+    :placeholder="label"
+    :tw-class="twClass"
+    :prepend-icon="prependIcon"
+    :disabled="disabled"
+  />
+</template>
+
+<script setup lang='ts'>
+import BaseInput, { type BaseInputProps } from '~/components/inputs/BaseInput.vue'
+
+defineComponent({ name: 'TextInput' })
+type TextInputProps = {
+  label: string
+} & Omit<BaseInputProps, 'type'>
+defineProps<TextInputProps>()
+
+const modelValue = defineModel<string>()
+</script>
