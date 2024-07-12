44316a5291285b6742e9c361648dd7c8c77b089e
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2024-07-12 19:47:04 +0200

    fix: add type to icon button

--- src/components/buttons/IconButton.vue
+++ src/components/buttons/IconButton.vue
@@ -1,6 +1,7 @@
 <template>
   <button
     :disabled="loading"
+    type="button"
     class="flex items-center gap-3 bg-primary-500 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded"
   >
     <FontAwesomeIcon
